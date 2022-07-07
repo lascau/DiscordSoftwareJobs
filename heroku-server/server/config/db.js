1c4d326c4908464b5f956b9058bdc7020c509a51
Lăscău Ionut Sebastian <lascauionutsebastian98@gmail.com>
2022-07-07 20:57:08 +0300

    sdsds

--- heroku-server/server/config/db.js
+++ heroku-server/server/config/db.js
@@ -1,21 +1,16 @@
 require("dotenv").config({ path: "../.env" });
 const { Sequelize } = require("sequelize");
 
-const db = new Sequelize(
-    "discord_jobs",
-    process.env.POSTGRES_USER,
-    process.env.POSTGRES_PW,
-    {
-        host: process.env.LOCAL_HOST,
-        dialect: "postgres",
+const db = new Sequelize(process.env.DATABASE_URL, {
+    host: process.env.LOCAL_HOST,
+    dialect: "postgres",
 
-        pool: {
-            max: 5,
-            min: 0,
-            acquire: 30000,
-            idle: 10000,
-        },
-    }
-);
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000,
+    },
+});
 
 module.exports = db;
