e51caf84e27ecb5f45a76caf94a4137c89b28be6
Lăscău Ionut Sebastian <lascauionutsebastian98@gmail.com>
2022-07-08 19:31:34 +0300

    lalala

--- server/index.js
+++ server/index.js
@@ -7,7 +7,13 @@ const port = process.env.PORT || 3009;
 
 app.use(express.json());
 
-app.use(cors());
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', 'https://discord-jobs-client.herokuapp.com');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+    });
 
 // Database Connection
 const db = require("./config/db");
