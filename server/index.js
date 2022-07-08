589bda327ed740fc804ee2a7c5533c38ead733ed
Lăscău Ionut Sebastian <lascauionutsebastian98@gmail.com>
2022-07-08 15:05:52 +0300

    removed one corsOption

--- server/index.js
+++ server/index.js
@@ -8,7 +8,6 @@ const port = process.env.PORT || 3009;
 app.use(express.json());
 var corsOptions = {
     origin: [ "https://discord-jobs-client.herokuapp.com"],
-    optionsSuccessStatus: 200 // For legacy browser support
 }
 app.use(cors(corsOptions));
 
