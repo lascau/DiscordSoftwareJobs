7d2430f653c4284ebc3b685f8cd487b8570c6f6d
Lăscău Ionut Sebastian <lascauionutsebastian98@gmail.com>
2022-07-09 10:55:50 +0300

    sdsdS

--- client/src/components/JobsCards.js
+++ client/src/components/JobsCards.js
@@ -25,7 +25,7 @@ const JobsCards = (props) => {
                                 jobDescription={job.content}
                                 avatarId={job.avatar_id}
                                 authorId={job.author_id}
-                                key={author + jobDescription}
+                                key={job.author + job.content}
                             />
                         </Box>
                     ))
